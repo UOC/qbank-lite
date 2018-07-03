e607cb06556ff3927573d3479017ca4da3c41cbc
Xavi Aracil <xaracil@uoc.edu>
2018-07-03 12:07:35 +0200

    JSON serialization of Decimal objects

--- utilities.py
+++ utilities.py
@@ -5,6 +5,7 @@ import web
 import os
 
 from datetime import date, datetime
+from decimal import Decimal
 from urllib import quote
 
 from dlkit.json_ import types
@@ -374,4 +375,6 @@ def json_serial(obj):
 
     if isinstance(obj, (datetime, date)):
         return obj.isoformat()
+    if isinstance(obj, Decimal):
+        return float(obj)
     raise TypeError ("Type %s not serializable" % type(obj))
